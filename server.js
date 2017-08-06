8ce52a7a3c9312729a009eef0e83c439a5a0d453
Rick Gallegos <duulketariakan@yahoo.com>
2017-08-05 17:54:57 -0700

    Server running

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var app = require('./letterapp.js')
-var port = 8080;
+//var app = require('./letterapp.js')
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'))
 
+console.log ('hi');
+var port = 8080;
 app.listen(port);
 console.log('Server now listening on port ' + port);
