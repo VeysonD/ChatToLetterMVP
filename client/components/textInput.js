642a903d06b68eab5b0519787f45c2e21b38d1ae
Rick Gallegos <duulketariakan@yahoo.com>
2017-08-06 22:37:20 -0700

    Imposed a limit of 10 characters or less for input

--- client/components/textInput.js
+++ client/components/textInput.js
@@ -1,7 +1,7 @@
 angular.module('write-letter')
   .controller('textInputController', function($scope, zoom) {
     this.handleClick = (event) => {
-      zoom.postMessage(this.input);
+      zoom.postMessage(this.input.slice(0, 10));
 
       zoom.retrieveMessages(messages => {
         this.messages(messages.data);
