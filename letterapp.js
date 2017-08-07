ad47c5891c0f94020870be4a8549f77556409503
Rick Gallegos <duulketariakan@yahoo.com>
2017-08-06 18:18:20 -0700

    Reusing mongodb

--- letterapp.js
+++ letterapp.js
@@ -29,36 +29,36 @@ var cleanText = function(text) {
 }
 
 //  MONGO DB MESSAGES
-// app.get('/messages', function(req, res, next) {
-//   console.log('GET message request', req);
-//   console.log('GET message response', res);
-//
-//   Message.find({}).exec(function(err, messages) {
-//     console.log('GET messages', messages);
-//     res.status(200).send(messages);
-//   });
-// })
-
-//ZOOM DB MESSAGES
 app.get('/messages', function(req, res, next) {
   console.log('GET message request', req);
   console.log('GET message response', res);
 
-  var options = {
-    qs: {
-      api_key: zoom_key,
-      api_secret: zoom_sec,
-      data_type: "JSON",
-      access_token: ,
-      session_id: ,
-      from: "2017-08-01",
-      to: "2017-08-06",
-    }
-  };
-  var syncReq = syncrequest('GET', "https://api.zoom.us/v1/chat/get", options);
-  var response = JSON.parse(syncReq.getBody('utf8'));
-  res.status(200).send(response);
+  Message.find({}).exec(function(err, messages) {
+    console.log('GET messages', messages);
+    res.status(200).send(messages);
+  });
 })
+
+//ZOOM DB MESSAGES
+// app.get('/messages', function(req, res, next) {
+//   console.log('GET message request', req);
+//   console.log('GET message response', res);
+//
+//   var options = {
+//     qs: {
+//       api_key: zoom_key,
+//       api_secret: zoom_sec,
+//       data_type: "JSON",
+//       access_token: ,
+//       session_id: ,
+//       from: "2017-08-01",
+//       to: "2017-08-06",
+//     }
+//   };
+//   var syncReq = syncrequest('GET', "https://api.zoom.us/v1/chat/get", options);
+//   var response = JSON.parse(syncReq.getBody('utf8'));
+//   res.status(200).send(response);
+//})
 
 app.post('/messages', function(req, res, next) {
   console.log('POST message request', req.body);
